6ea3111522847cdc9dea68aad34bd10d8c2b5f23
Linyang He <leonlovesjazz@gmail.com>
2018-06-06 21:00:09 +0800

    UPDATE

--- Social_Network_Part_HW/classification.py
+++ Social_Network_Part_HW/classification.py
@@ -34,11 +34,13 @@ def get_personas_count(prediction_personas):
 
 # user_matrix_train, user_matrix_test = split_data()
 
-def matrix_process():
-    user_matrix_new = np.zeros((8767, 884))
+def matrix_process(user_matrix):
+    M = user_matrix.shape[0]
+    N = user_matrix.shape[1]
+    user_matrix_new = np.zeros((M, N))
 
-    for i in range(8767):
-        for j in range(884):
+    for i in range(M):
+        for j in range(N):
             temp = user_matrix[i][j]
             if temp == temp:
                 user_matrix_new[i][j] = temp
@@ -50,26 +52,32 @@ def matrix_process():
 # prediction_personas = global_KMeans(20, user_matrix_new)
 # personas_count = get_personas_count(prediction_personas)
 
-# #Model 2 - KMeans with PCA dimensionality reduction(LSA)
-# K = 20
-# data_pca = PCA(n_components=50).fit_transform(user_matrix_new)
-# prediction_personas_pca = global_KMeans(K, data_pca)
-# personas_count_pca = get_personas_count(prediction_personas_pca)
+#Model 2 - KMeans with PCA dimensionality reduction(LSA)
+user_matrix_new_stop = matrix_process(user_matrix_stop)
+K = 4
+data_pca_stop = PCA(n_components=50).fit_transform(user_matrix_new_stop)
+prediction_personas_pca_stop = global_KMeans(K, data_pca_stop)
+personas_count_pca_stop = get_personas_count(prediction_personas_pca_stop)
 
 #Compute the most influential label TF-IDF
-def get_words_lists():
+def get_words_lists(personas_count_pca, user_df):
+    label_list = user_df.columns
+    label_num = user_df.shape[1]
     words_lists = []
     for user_lists in personas_count_pca:
         words_list = {}
         for user_index in personas_count_pca[user_lists]:
-            for i in range(20):
-                try:
-                    words_list[tag_lists.iloc[user_index][i]] += 1
-                except:
-                    words_list[tag_lists.iloc[user_index][i]] = 1
+            for i in range(label_num):
+                temp = user_df.iloc[user_index][i]
+                if temp:
+                    try:
+                        words_list[label_list[i]] += 1
+                    except:
+                        words_list[label_list[i]] = 1
         words_lists.append(words_list)
     return words_lists
-words_lists = get_words_lists()
+
+words_lists_stop = get_words_lists(personas_count_pca_stop, user_df_stop)
 
                  
 
