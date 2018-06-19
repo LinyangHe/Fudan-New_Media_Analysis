e2589c77a0f9d681837c70e9cc6f20343a856cc7
Linyang He <leonlovesjazz@gmail.com>
2018-06-19 19:02:33 +0800

    Update

--- Final_Project/load_word2vec.py
+++ Final_Project/load_word2vec.py
@@ -5,11 +5,17 @@ import numpy as np
 # # word2vec_dic = pandas.read_table('test.txt',sep=' ')
 # # print(word2vec_dic)
 
-word2vec_dic = {}
-with open('D:/NLP/Data/word2vec/sgns.financial.bigram', encoding='latin-1') as file:
-	file.readline()
-	for line in file:
-		if not line:
-			continue
-		line = line.strip().split(' ')
-		word2vec_dic[line[0]] = np.array([float(i) for i in line[1:]])
+def main():
+	word2vec_dic = {}
+	with open('D:/NLP/Data/word2vec/sgns.financial.bigram', encoding='utf-8', errors='ignore') as file:
+		file.readline()
+		for line in file:
+			if not line:
+				continue
+			line = line.strip().split(' ')
+			word2vec_dic[line[0]] = np.array([float(i) for i in line[1:]])
+
+if __name__ == '__main__':
+	main()
+
+
--- Final_Project/util.py
+++ Final_Project/util.py
@@ -5,6 +5,7 @@ import numpy as np
 import sqlite3
 import jieba
 import sklearn
+import copy
 
 def load_database(db):
     conn = sqlite3.connect(db)
@@ -21,8 +22,73 @@ def load_news():
 	    'C:/Users/Leon/OneDrive/DataScience/10_New Media/Project/Data/opinion2.db')
 	news = pd.concat([news_1, news_2])
 	return news
-news = load_news()
-news_head = news.head()
-print(news_head)
 
-for 
+# news = load_news()
+news_head = news.head(10000)
+
+# day_news_data = pd.DataFrame(columns=['date','news','word2vec','sh_next_day'])
+def get_day_news_data():
+    day_news_data = {}
+    today = news_head.iloc[0]['time'].strip().split(' ')[0]
+    for i in range(len(news_head)):
+        time = news_head.iloc[i]['time']
+        day = time.strip().split(' ')[0]
+
+        if day > today:
+            yesterday = copy.deepcopy(today)
+            today = day
+            print(yesterday, today)
+
+        closed_time = day + " 15:00:00.000000"
+        try:
+            whole_news = news_head.iloc[i]['title'] + news_head.iloc[i]['title'] + news_head.iloc[i]['content']
+        except:
+            whole_news = news_head.iloc[i]['title'] + news_head.iloc[i]['title']        
+
+        try:
+            if time > closed_time:
+                try:
+                    day_news_data[day] += whole_news
+                except KeyError:
+                    day_news_data[day] = whole_news
+            else:
+                try:
+                    day_news_data[yesterday] += whole_news
+                except KeyError:
+                    day_news_data[day] = whole_news
+        except IndexError:
+            pass
+    
+    return day_news_data
+
+def get_day_news_vec(day_news_data):
+    day_news_vec = {}
+    for day in day_news_data:
+        print(day)
+        news_split = [i for i in jieba.cut(day_news_data[day])]
+        word_num = 0
+        doc2vec = np.zeros((300))
+        for i in news_split:
+            try:
+                doc2vec += word2vec_dic[i]
+                # print(doc2vec)
+                word_num += 1
+            except KeyError:
+                pass
+        doc2vec /= word_num
+        day_news_vec[day] = doc2vec
+            
+    return day_news_vec, news_split
+    
+def get_day_sh_data(day_news_data):
+    day_sh_data = {}
+    sh_data = pd.read_csv('./Data/sh.csv')
+    for i in range(len(day_news_data)):
+        pass
+
+
+
+day_news_data = get_day_news_data()
+day_news_vec, new_split = get_day_news_vec(day_news_data)
+
+
